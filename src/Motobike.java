eb43a0e84bb48d150a4405214826e442a883c5be
ĐINH ANH ĐỨC <dinhanhduc2006@gmail.com>
2025-12-21 15:34:04 +0700

    [REFACTOR] Removing hardcode and implement BigDecimal

--- src/Motobike.java
+++ src/Motobike.java
@@ -1,10 +1,14 @@
+import java.math.BigDecimal;
 class Motobike extends Vehicle {
 
-  int engineCapacity;
-  String typeMotobike;
-  String power;
-
-  public Motobike(String model, String manufacturer, int year, double basePrice, String origin,
+  private int engineCapacity;
+  private String typeMotobike;
+  private String power;
+  private static final String ORIGIN_DOMESTIC = "domestic";
+  private static final BigDecimal IMPORT_TAX_RATE = BigDecimal.valueOf(0.3);
+  private static final BigDecimal EXCISE_TAX_RATE = BigDecimal.valueOf(0.2);
+  private static final int ENGINE_CAPACITY_LIMIT = 150;
+  public Motobike(String model, String manufacturer, int year, BigDecimal basePrice, String origin,
       int engineCapacity, String typeMotobike, String power) {
     super(model, manufacturer, year, basePrice, origin);
     this.engineCapacity = engineCapacity;
@@ -14,21 +18,21 @@ class Motobike extends Vehicle {
   }
 
   @Override
-  double importTax() {
-    if (origin.equals("domestic")) {
-      return 0;
+  BigDecimal importTax() {
+    if (origin.equalsIgnoreCase(ORIGIN_DOMESTIC)) {
+      return BigDecimal.ZERO;
     } else {
-      return basePrice * 0.3;
+      return basePrice.multiply(IMPORT_TAX_RATE);
     }
   }
 
   @Override
-  double exciseTax() {
-    if (engineCapacity < 150) {
-      return 0;
+  BigDecimal exciseTax() {
+    if (engineCapacity < ENGINE_CAPACITY_LIMIT) {
+      return BigDecimal.ZERO;
     } else {
-      return (basePrice + importTax()) * 0.2;
+      BigDecimal taxBase = basePrice.add(importTax());
+      return taxBase.multiply(EXCISE_TAX_RATE);
     }
-
   }
 }
