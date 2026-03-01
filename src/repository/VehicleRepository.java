f30a675e5b4c4c957d2a94763c90e4cfb44f4370
ĐINH ANH ĐỨC <dinhanhduc2006@gmail.com>
2026-03-01 16:09:06 +0700

    [FEAT] Initiating the connection by JDBC

---
+++ src/repository/VehicleRepository.java
@@ -0,0 +1,26 @@
+package repository;
+
+import java.util.ArrayList;
+
+public class VehicleRepository implements RepositoryInterface{
+
+  @Override
+  public int insert(Object object) {
+    return 0;
+  }
+
+  @Override
+  public int update(Object object) {
+    return 0;
+  }
+
+  @Override
+  public int delete(Object object) {
+    return 0;
+  }
+
+  @Override
+  public ArrayList selectAll() {
+    return null;
+  }
+}
